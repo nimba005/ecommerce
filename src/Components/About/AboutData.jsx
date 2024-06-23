755008133b79e3ab7e342ee11e21f98cb1eafc71
nimba005 <nimbaoyugi@gmail.com>
2024-06-23 13:50:39 +0300

    #added AboutData file

---
+++ src/Components/About/AboutData.jsx
@@ -0,0 +1,51 @@
+import ease from '../../Assets/ease.png';
+import access from '../../Assets/access.png';
+import backup from '../../Assets/backup.jpg';
+import paperwork from '../../Assets/paperwork.jpeg';
+import accounting from '../../Assets/accounting.jpg';
+import security from '../../Assets/security.jpg';
+import cost from '../../Assets/cost.png';
+import userfriendly from '../../Assets/user.png';
+
+export const datas = [
+  {
+    img: ease,
+    title:"Ease",
+    text: "Ease in management of rentals, tenants' caretaking, and even space management and accounting. You do not have to stress yourself nor do you have to be present at all times to mange your rentals."
+  },
+  {
+    img: access,
+    title: "Acesss",
+    text: "You have access to your rentals from virtually anywhere in the world. You have access to information about the property, vacancies, or even payments at any time of the day. Accessibility is enjoyed to the maximum."
+  },
+  {
+    img: paperwork,
+    title: "Reduced/No Paperwork",
+    text: "There is little or no need for paperwork. Any relevant information about your rentals, such as tenants and their payments, will be stored in your system."
+  },
+  {
+    img: backup,
+    title: "Backup and Recovery",
+    text: "In case of an accident, you have little or no chance of losing any of the virtual information that you have gathered and kept over the years. This cannot be said for paperwork."
+  },
+  {
+    img: accounting,
+    title: "Accounting Services",
+    text: "It will be very easy to track your payments through our system. This will help you track every payment for your rentals. You will be able to know which house is paid and which one is not."
+  },
+  {
+    img: security,
+    title: "Security",
+    text: "Unlike paperwork, every piece of information will be safely stored in our system. Any payment or any other information will be kept safe."
+  },
+  {
+    img: cost,
+    title: "Reduced Cost",
+    text: "When you make use of our system, you will get to save more and avoid unnecessary costs. You can cut costs on transportation and save more when you have total control of your finances."
+  },
+  {
+    img: userfriendly,
+    title: "Friendly User Interface",
+    text: "Our syatem is made with a friendly user interface such that you can easily navigate your way around it and also do things with little or no expertise."
+  }
+];
