1f6a54dc8833d2f9232fff31230d65ddffde868f
nimba005 <nimbaoyugi@gmail.com>
2024-06-26 14:24:20 +0300

    #added the App components and env

--- src/App.js
+++ src/App.js
@@ -1,2 +1,25 @@
-import { Routes, Route } from 'react-router';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./Components/Home/Home";
+import Privacy from './Components/Privacy/Privacy';
+import Terms from './Components/Terms/Terms';
+import Pricing from './Components/Pricing/Pricing';
+import Footer from './Components/Footer/Footer';
+import { BASE_URL } from './Components/Config/Config';
+
+function App() {
+  console.log('Base URL:', BASE_URL);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy-policy" element={<Privacy />} />
+        <Route path="/terms-and-conditions" element={<Terms />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+      <footer />
+    </>
+  );
+}
+
+export default App;
