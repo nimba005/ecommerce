e171b63953e1bdbe336252e51488ea45b52adb60
nimba005 <nimbaoyugi@gmail.com>
2024-06-26 14:32:13 +0300

    #webvitalis added

---
+++ src/reportWebVitals.js
@@ -0,0 +1,13 @@
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getLCP(onPerfEntry);
+      getFCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
