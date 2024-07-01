e8794029b373dc8926777c24ca0ad18b574cfa75
nimba005 <nimbaoyugi@gmail.com>
2024-07-01 15:06:35 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./Components/Home/Home.jsx";
-import About from './Components/About/About.jsx';
-import Contact from './Components/Contact/Contact.jsx';
-import Privacy from './Components/Privacy/Privacy.jsx';
-import Terms from './Components/Terms/Terms.jsx';
-import Pricing from './Components/Pricing/Pricing.jsx';
-import Testimonials from './Components/Testimonials/Testimonials.jsx';
-import { BASE_URL } from './Components/Config/Config.js';
+import Home from "./Components/Home/Home";
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Privacy from './Components/Privacy/Privacy';
+import Terms from './Components/Terms/Terms';
+import Pricing from './Components/Pricing/Pricing';
+import Testimonials from './Components/Testimonials/Testimonials';
+import { BASE_URL } from './Components/Config/Config';
 
 function App() {
   console.log('Base URL:', BASE_URL);
